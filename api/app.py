86199a494473060f001d87189d36edec5ed786c1
Mostafa Elgamal <123297870+MostafaElgamal2030@users.noreply.github.com>
2025-05-08 02:23:56 +0300

    New_dataset

--- api/app.py
+++ api/app.py
@@ -1,25 +1,77 @@
 from flask import Flask, request, jsonify
 import numpy as np
+from sklearn.metrics import accuracy_score
 from api.model_loader import load_model
 
 app = Flask(__name__)
-model = load_model()  # Load from joblib
+random_forest_model = load_model(model_path="models/HC_RandomForestModel.joblib")
+#xg_boost_model_model = load_model(model_path="models/HC_XgBoostModel.joblib")
 
-@app.route('/predict', methods=['POST'])
+@app.route('/randomforest/predict', methods=['POST'])
 def predict():
     try:
 
         data = request.json
+    
+        # Extract input features
         features = np.array(data['input'])
 
-        # Ensure features is 2D (supports both 1D and 2D input)
+        # Ensure 2D shape
         if features.ndim == 1:
             features = features.reshape(1, -1)
 
-        prediction = model.predict(features)
-        return jsonify({'prediction': prediction.tolist()})
+        # Make prediction
+        prediction = random_forest_model.predict(features)
+
+        # If expected output is provided, calculate accuracy
+        if 'expected_output' in data:
+            expected_output = np.array(data['expected_output'])
+            accuracy = accuracy_score(expected_output, prediction)
+            return jsonify({
+                'prediction': prediction.tolist(),
+                'expected_output': expected_output.tolist(),
+                'accuracy': accuracy
+            })
+
+        # If no expected output, return prediction only
+        return jsonify({
+            'prediction': prediction.tolist()
+        })
     except Exception as e:
         return jsonify({'error': str(e)}), 400
+
+# @app.route('/xgboost/predict', methods=['POST'])
+# def predict():
+#     try:
+
+#         data = request.json
+    
+#         # Extract input features
+#         features = np.array(data['input'])
+
+#         # Ensure 2D shape
+#         if features.ndim == 1:
+#             features = features.reshape(1, -1)
+
+#         # Make prediction
+#         prediction = random_forest_model.predict(features)
+
+#         # If expected output is provided, calculate accuracy
+#         if 'expected_output' in data:
+#             expected_output = np.array(data['expected_output'])
+#             accuracy = accuracy_score(expected_output, prediction)
+#             return jsonify({
+#                 'prediction': prediction.tolist(),
+#                 'expected_output': expected_output.tolist(),
+#                 'accuracy': accuracy
+#             })
+
+#         # If no expected output, return prediction only
+#         return jsonify({
+#             'prediction': prediction.tolist()
+#         })
+#     except Exception as e:
+#         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
